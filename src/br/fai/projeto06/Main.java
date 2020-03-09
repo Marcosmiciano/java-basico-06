9fc87177954f612465754923a6334ccde1a8fe7a
marcosmiciano <miciano.marcos@gmail.com>
2020-03-09 20:02:26 -0300

    Resolução do problema

--- src/br/fai/projeto06/Main.java
+++ src/br/fai/projeto06/Main.java
@@ -12,11 +12,41 @@ public class Main {
 	private void start() {
 		String[] carros = {"civic","gol","palio","uno"};
 							//0     1       2      3
-		System.out.println("Posição 1: " + carros[1]);
+		//System.out.println("Posição 1: " + carros[1]);
 		
 		for(String carro : carros) {
 			
 			System.out.println("Carro: " + carro);
 		}
+		
+		System.out.println("--------");
+		
+
+		
+		carros[1] = "L200";
+		for(String carro : carros) {
+			
+			System.out.println("Carro: " + carro);
+		}
+		
+		int tamanhoDoNovoArray = carros.length + 1;
+		
+		String[] novoArrayDeCarros =
+				new String[tamanhoDoNovoArray];
+		
+		for(int i = 0; i < carros.length; i++) {
+			String carro = carros[i];
+			novoArrayDeCarros[i] = carro;
+			
+		}
+		novoArrayDeCarros [tamanhoDoNovoArray - 1] = "gol";
+		for(String carro : novoArrayDeCarros) {
+			
+			System.out.println("Carro: " + carro);
+			}
+		
+		}
+		
+		 
 	}
-}
+
