30556050e244de25042346cfbe9d2867b0bb3054
marcosmiciano <miciano.marcos@gmail.com>
2020-03-09 19:42:57 -0300

    Exercicios a e b

--- src/br/fai/projeto06/Main.java
+++ src/br/fai/projeto06/Main.java
@@ -3,14 +3,20 @@ package br.fai.projeto06;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Main app = new Main();
 		app.start();
 
 	}
 
 	private void start() {
-	
+		String[] carros = {"civic","gol","palio","uno"};
+							//0     1       2      3
+		System.out.println("Posição 1: " + carros[1]);
 		
+		for(String carro : carros) {
+			
+			System.out.println("Carro: " + carro);
+		}
 	}
-
 }
